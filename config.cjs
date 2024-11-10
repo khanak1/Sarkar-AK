6d1388418bd7a4dee52a99e4021a395f1105a457
khanak1 <144924515+khanak1@users.noreply.github.com>
2024-11-11 00:48:20 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "session",
+  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&WvrfSqMp",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true", 
   AUTO_DL: process.env.AUTO_DL || "true",
